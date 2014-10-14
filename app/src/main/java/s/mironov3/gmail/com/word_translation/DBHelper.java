b3f40cb15a54c1e63cc5f2e453b83fe9ea9f6bd9
Sergey Mironov <ka6ah505@gmail.com>
2014-10-14 18:01:03 +0400

    created DBHelper class

---
+++ app/src/main/java/s/mironov3/gmail/com/word_translation/DBHelper.java
@@ -0,0 +1,104 @@
+package s.mironov3.gmail.com.word_translation;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    // Поля и название таблицы с темами набора слов
+    public static final String TABLE_THEME = "theme";
+    public static final String THEME_ID = "id";
+    public static final String THEME_NAME = "name";
+
+    // Поля и название таблицы с парами слов "слово-первод"
+    public static final String TABLE_WORD = "translate";
+    public static final String TRANSLATE_ID = "id";
+    public static final String WORD = "word";
+    public static final String TRANSLATE = "translate";
+    public static final String THEME_TRANSLATE_ID = "thId";
+
+    // временные данные
+    String[] nameTheme = { "FAMILY", "CITY", "COLOURS", "SPORT" };
+
+    String[] wordEnglish = { "baby", "brother", "red", "soccer", "street", "pavement" };
+    String[] wordTranslate = { "ребёнок", "брат", "красный", "футбол", "улица", "рекламный щит" };
+    int[] word_posid = { 1, 1, 3, 4, 2, 2 };
+
+    public DBHelper(Context context) {
+        // конструктор суперкласса
+        super(context, "myDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        ContentValues cv = new ContentValues();
+
+        // создаем таблицу с названием темы
+        db.execSQL("create table " + TABLE_THEME + " ("
+                + THEME_ID + " integer primary key autoincrement,"
+                + THEME_NAME + " text"
+                +  ");");
+        // заполняем таблицу
+        for(int i = 0; i < nameTheme.length; i++ ) {
+            cv.clear();
+            cv.put(THEME_NAME, nameTheme[i]);
+            db.insert(TABLE_THEME, null, cv);
+        }
+
+        // создаём таблицу с парой слово-перевод
+        db.execSQL("create table " + TABLE_WORD + " ("
+                + TRANSLATE_ID + " integer primary key autoincrement,"
+                + WORD + " text,"
+                + TRANSLATE + " text,"
+                + THEME_TRANSLATE_ID + " integer"
+                +  ");");
+        // заполнение таблицы
+        for(int i = 0; i < wordEnglish.length; i++) {
+            cv.clear();
+            cv.put(WORD, wordEnglish[i]);
+            cv.put(TRANSLATE, wordTranslate[i]);
+            cv.put(THEME_TRANSLATE_ID, word_posid[i]);
+            db.insert(TABLE_WORD, null, cv);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public static String getTableTheme() {
+        return TABLE_THEME;
+    }
+
+    public static String getThemeId() {
+        return THEME_ID;
+    }
+
+    public static String getThemeName() {
+        return THEME_NAME;
+    }
+
+    public static String getTableWord() {
+        return TABLE_WORD;
+    }
+
+    public static String getTranslateId() {
+        return TRANSLATE_ID;
+    }
+
+    public static String getWord() {
+        return WORD;
+    }
+
+    public static String getTranslate() {
+        return TRANSLATE;
+    }
+
+    public static String getThemeTranslateId() {
+        return THEME_TRANSLATE_ID;
+    }
+}
--- app/src/main/java/s/mironov3/gmail/com/word_translation/MainActivity.java
+++ app/src/main/java/s/mironov3/gmail/com/word_translation/MainActivity.java
@@ -1,7 +1,10 @@
 package s.mironov3.gmail.com.word_translation;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +15,39 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBHelper dbHelper = new DBHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor cursor;
+        Log.d("MYLOG", "----------TABLE THEME------------");
+        cursor = db.query(dbHelper.getTableTheme(), null, null, null, null, null, null);
+        logCursor(cursor);
+        cursor.close();
+        Log.d("MYLOG", "---------------------------------");
+
+        Log.d("MYLOG", "----------TABLE WORD------------");
+        cursor = db.query(dbHelper.getTableWord(), null, null, null, null, null, null);
+        logCursor(cursor);
+        cursor.close();
+        Log.d("MYLOG", "---------------------------------");
+    }
+
+    private void logCursor(Cursor cursor) {
+        if( cursor != null ) {
+            if(cursor.moveToFirst()){
+                String str;
+                do {
+                    str = "";
+                    for( String cn: cursor.getColumnNames() ) {
+                        str = str.concat(cn + " = " + cursor.getString(cursor.getColumnIndex(cn)) + "; ");
+                    }
+                    Log.d("MYLOG", str);
+                } while (cursor.moveToNext());
+            }
+        } else {
+            Log.d("MYLOG", "BEDA!!!");
+        }
     }
 
 
